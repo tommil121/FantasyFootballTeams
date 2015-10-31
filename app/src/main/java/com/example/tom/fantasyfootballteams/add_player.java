42ab022e26521bc737d54ee587c4c67d580e8b69
Dustin <dustin2094@gmail.com>
2015-10-31 15:01:26 -0400

    Deleted the envelope icon and functionality on add player page

--- app/src/main/java/com/example/tom/fantasyfootballteams/add_player.java
+++ app/src/main/java/com/example/tom/fantasyfootballteams/add_player.java
@@ -21,15 +21,7 @@ public class add_player extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
