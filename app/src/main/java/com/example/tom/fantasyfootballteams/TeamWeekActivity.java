7fbcac3f45a5b5b2319a45d008caa662258fad9d
Tom <tommil121@gmail.com>
2015-11-10 10:34:17 -0500

    added a team_week activity, purpose being to select team and week from spinners.  Its not working as is.  Having issues with crashing on pages except for main load screen and create team

---
+++ app/src/main/java/com/example/tom/fantasyfootballteams/TeamWeekActivity.java
@@ -0,0 +1,61 @@
+package com.example.tom.fantasyfootballteams;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class TeamWeekActivity extends AppCompatActivity {
+
+    Intent intent;
+
+    DBHandler dbHandler;
+    Spinner teamNameSpinner;
+
+    //For team spinner
+    Team [] teams;
+    ArrayList<String> teamList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_week);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        teamNameSpinner = (Spinner)findViewById(R.id.select_team_spinner);
+
+        dbHandler = new DBHandler(this, null);
+
+        //put all of the teams in an array of Team objects
+        teams = dbHandler.getTeams();
+
+        //declare and initialize an array list for strings
+        teamList = new ArrayList<String>();
+
+
+        //go through the list of Team objects and extract the "list friendly" toString method and
+        //add them to the array list
+        for(int i = 0; i < teams.length; i++){
+            teamList.add(teams[i].toStringPlayer());
+        }
+
+        //create an adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, teamList);
+
+        //set the adapter to fill the list
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //apply the adapter to the spinner
+        teamNameSpinner.setAdapter(adapter);
+    }
+
+}
