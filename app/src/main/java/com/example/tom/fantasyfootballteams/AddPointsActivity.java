4bc6c00e95ad7aadfa13d2d9c145de109bf9abad
Tom <tommil121@gmail.com>
2015-11-17 10:19:19 -0500

    ...

---
+++ app/src/main/java/com/example/tom/fantasyfootballteams/AddPointsActivity.java
@@ -0,0 +1,43 @@
+package com.example.tom.fantasyfootballteams;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class AddPointsActivity extends AppCompatActivity {
+
+    TextView team;
+    String teamName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_points);
+
+        Team fakeTeam = new Team("Fake", "2014", "NFC");
+        teamName = fakeTeam.getTeamName();
+        team = (TextView)findViewById(R.id.teamTextView);
+        team.setText(teamName);
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
