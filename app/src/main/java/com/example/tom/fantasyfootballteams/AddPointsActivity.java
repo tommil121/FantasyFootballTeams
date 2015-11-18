9210746e788b11e85df7db0112929bd0cdf3c1f5
Tom <tommil121@gmail.com>
2015-11-17 21:13:13 -0500

    Added a bunch of dummy data to get a better idea of what the Add points ui will look like.  Also messed around more with the teamWeek Activity UI

--- app/src/main/java/com/example/tom/fantasyfootballteams/AddPointsActivity.java
+++ app/src/main/java/com/example/tom/fantasyfootballteams/AddPointsActivity.java
@@ -8,10 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class AddPointsActivity extends AppCompatActivity {
 
     TextView team;
     String teamName;
+    TextView qb;
+    String qbName;
+    TextView rb;
+    String rbName;
+    TextView wr;
+    String wrName;
+    TextView te;
+    String teName;
+    TextView k;
+    String kName;
+    TextView dst;
+    String dstName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +33,41 @@ public class AddPointsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_points);
 
         //fakeTeam eventually becomes return from roster table
-        Team fakeTeam = new Team("Fake", "2014", "NFC");
-        teamName = fakeTeam.getTeamName();
+        Team maury = new Team("Maury", "2015", "NFC");
+        teamName = maury.getTeamName();
         team = (TextView)findViewById(R.id.teamTextView);
         team.setText(teamName);
 
+        //added dummy data to check ui
+        Player qbPlayer = new Player("Maury Povich", "Maury", "QB");
+        qbName = qbPlayer.getName();
+        qb = (TextView)findViewById(R.id.qbTextView);
+        qb.setText(qbName);
 
+        Player rbPlayer = new Player("Jerry Springer", "Maury", "RB");
+        rbName = rbPlayer.getName();
+        rb = (TextView)findViewById(R.id.rbTextView);
+        rb.setText(rbName);
+
+        Player wrPlayer = new Player("Steve Wilkos", "Maury", "WR");
+        wrName = wrPlayer.getName();
+        wr = (TextView) findViewById(R.id.wrTextView);
+        wr.setText(wrName);
+
+        Player tePlayer = new Player("Montell Williams", "Maury", "TE");
+        teName = tePlayer.getName();
+        te = (TextView)findViewById(R.id.teTextView);
+        te.setText(teName);
+
+        Player kPlayer = new Player("Oprah", "Maury", "K");
+        kName = kPlayer.getName();
+        k = (TextView) findViewById(R.id.kTextView);
+        k.setText(kName);
+
+        Player dstPlayer = new Player("Ellen", "Maury", "DST");
+        dstName = dstPlayer.getName();
+        dst = (TextView) findViewById(R.id.dstTextView);
+        dst.setText(dstName);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
