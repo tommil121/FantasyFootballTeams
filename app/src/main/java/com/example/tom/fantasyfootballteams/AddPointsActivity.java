0146860eaeec97ef2a751af4b764eff932b65168
Tom <tommil121@gmail.com>
2015-11-18 21:52:02 -0500

    changed addpoints java to handle errors

--- app/src/main/java/com/example/tom/fantasyfootballteams/AddPointsActivity.java
+++ app/src/main/java/com/example/tom/fantasyfootballteams/AddPointsActivity.java
@@ -6,34 +6,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class AddPointsActivity extends AppCompatActivity {
 
-    TextView team;
-    String teamName;
+    //vars for handling names
+    TextView team, qb, rb, wr, te, k, dst, weekNum;
+    String teamName, qbName, rbName, wrName, teName,
+            kName, dstName;
 
-    TextView weekNum;
-
-    TextView qb;
-    String qbName;
-
-    TextView rb;
-    String rbName;
-
-    TextView wr;
-    String wrName;
-
-    TextView te;
-    String teName;
-
-    TextView k;
-    String kName;
-
-    TextView dst;
-    String dstName;
+    //create EditText variables for score input
+    EditText qbEditText, rbEditText, wrEditText, teEditText,
+            kEditText, dstEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +69,14 @@ public class AddPointsActivity extends AppCompatActivity {
         dst = (TextView) findViewById(R.id.dstTextView);
         dst.setText(dstName);
 
+        //set qbEditText var to equal the text in QBPts editText input
+        qbEditText = (EditText) findViewById(R.id.QBPts);
+        rbEditText = (EditText) findViewById(R.id.RBPts);
+        wrEditText = (EditText) findViewById(R.id.WRPts);
+        teEditText = (EditText) findViewById(R.id.TEPts);
+        kEditText = (EditText) findViewById(R.id.KPts);
+        dstEditText = (EditText) findViewById(R.id.DSTPts);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -99,44 +95,41 @@ public class AddPointsActivity extends AppCompatActivity {
     //for each of the 6 players and sum them, then display this sum on screen
     public void calcPoints(View view) {
 
-        //create TextView variables
-        TextView qb, rb, wr, te, k, dst, total;
+        TextView total;
 
         //create int variables which will be totaled
-        int qbPts, rbPts, wrPts, tePts, kPts, dstPts;
-        //set int total_pts to 0
-        int total_pts = 0;
+        int qbPts, rbPts, wrPts, tePts, kPts, dstPts, total_pts;
 
-        //set TextView var qb to equal the text in QBPts editText input
-        qb = (TextView) findViewById(R.id.QBPts);
-        //convert the string value in qb to integer, store it in int var qbPts
-        qbPts = Integer.parseInt(qb.getText().toString());
+        String qb = qbEditText.getText().toString();
+        String rb = rbEditText.getText().toString();
+        String wr = wrEditText.getText().toString();
+        String te = teEditText.getText().toString();
+        String k = kEditText.getText().toString();
+        String dst = dstEditText.getText().toString();
 
-        rb = (TextView) findViewById(R.id.RBPts);
-        rbPts = Integer.parseInt(rb.getText().toString());
+        if (qb.trim().equals("") || rb.trim().equals("") || wr.trim().equals("") ||
+                te.trim().equals("") || k.trim().equals("") || dst.trim().equals("")) {
+            Toast.makeText(this, "Please enter the number of points scored Each Player!",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            //convert the string value in qb to integer, store it in int var qbPts
+            qbPts = Integer.parseInt(qb);
+            rbPts = Integer.parseInt(rb);
+            wrPts = Integer.parseInt(wr);
+            tePts = Integer.parseInt(te);
+            kPts = Integer.parseInt(k);
+            dstPts = Integer.parseInt(dst);
 
-        wr = (TextView) findViewById(R.id.WRPts);
-        wrPts = Integer.parseInt(wr.getText().toString());
+            //sum the integer values, store in int var total_pts
+            total_pts = (qbPts + rbPts + wrPts + tePts + kPts + dstPts);
+            //set TextView var total to equal totalTextView text box
+            total = (TextView) findViewById(R.id.totalTextView);
 
-        te = (TextView) findViewById(R.id.TEPts);
-        tePts = Integer.parseInt(te.getText().toString());
-
-        k = (TextView) findViewById(R.id.KPts);
-        kPts = Integer.parseInt(k.getText().toString());
-
-        dst = (TextView) findViewById(R.id.DSTPts);
-        dstPts = Integer.parseInt(dst.getText().toString());
-
-        //sum the integer values, store in int var total_pts
-        total_pts = (qbPts + rbPts + wrPts + tePts + kPts + dstPts);
-        //set TextView var total to equal totalTextView text box
-        total = (TextView)findViewById(R.id.totalTextView);
-
-        //code below ""+ takes the integer value from the int and converts it to
-        //String so that it can be placed in the editText box
-        //Does the same as String.valueOf(total_pts)
-        total.setText("" + total_pts);
-
+            //code below ""+ takes the integer value from the int and converts it to
+            //String so that it can be placed in the editText box
+            //Does the same as String.valueOf(total_pts)
+            total.setText("" + total_pts);
+        }
     }
 
 }
